038ad91844d0fd9eee9c6b306da312b46b6ff2da
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-12-03 16:15:39 +0600

    Lexicographical Order

---
+++ lexicographical.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    val words = arrayOf("MD", "HASAN", "AHNAF", "TAHMID")
+
+    for (i in 0..2) {
+        for (j in i + 1..3) {
+            if (words[i].compareTo(words[j]) > 0) {
+
+                // swap words[i] with words[j[
+                val temp = words[i]
+                words[i] = words[j]
+                words[j] = temp
+            }
+        }
+    }
+
+    println("In lexicographical order:")
+    for (i in 0..3) {
+        println(words[i])
+    }
+}
