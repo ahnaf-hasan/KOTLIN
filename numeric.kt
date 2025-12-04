2a851f43eb62fbc21207722538f8bf887fe71c92
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-12-04 15:45:14 +0600

    Numeric

---
+++ numeric.kt
@@ -0,0 +1,18 @@
+import java.lang.Double.parseDouble
+
+fun main(args: Array<String>) {
+
+    val string = "20262027"
+    var numeric = true
+
+    try {
+        val num = parseDouble(string)
+    } catch (e: NumberFormatException) {
+        numeric = false
+    }
+
+    if (numeric)
+        println("$string is a number")
+    else
+        println("$string is not a number")
+}
