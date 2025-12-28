77e45b46802bf7c362ebb263918cfb5eba86f891
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-12-28 13:50:07 +0600

    Leap Year or Not

---
+++ leap.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    val year = 2025
+    var leap = false
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            // year is divisible by 400, hence the year is a leap year
+            leap = year % 400 == 0
+        } else
+            leap = true
+    } else
+        leap = false
+
+    println(if (leap) "$year is a leap year." else "$year is not a leap year.")
+
+}
